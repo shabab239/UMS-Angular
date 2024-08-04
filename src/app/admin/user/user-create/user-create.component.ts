c8793ea0059d1eedf62e5316df3f632695681b15
Shabab Ahmed <sabab239.1@gmail.com>
2024-08-04 22:04:14 +0600

    Create User

--- src/app/admin/user/user-create/user-create.component.ts
+++ src/app/admin/user/user-create/user-create.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {User} from "../model/user.model";
+import {UserService} from "../user.service";
 
 @Component({
   selector: 'app-user-create',
@@ -7,8 +9,49 @@ import {Component, OnInit} from '@angular/core';
 })
 export class UserCreateComponent implements OnInit {
 
-  ngOnInit(): void {
+  user!: User;
+
+  constructor(private userService: UserService) {
   }
 
+  ngOnInit(): void {
+    this.user = new User();
+  }
+
+  handleFileInput(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      this.convertFileToBase64(file).then((base64: string) => {
+        this.user.avatar = base64;
+      });
+    }
+  }
+
+  private convertFileToBase64(file: File): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => {
+        resolve(reader.result as string);
+      };
+      reader.onerror = (error) => {
+        reject(error);
+      };
+      reader.readAsDataURL(file);
+    });
+  }
+
+  //todo set status, instId, check base64 encoding
+  createUser(): void {
+    this.userService.addUser(this.user).subscribe({
+      next: response => {
+        console.log('User created successfully:', response);
+        // Handle successful user creation
+      },
+      error: error => {
+        console.error('Error creating user:', error);
+        // Handle error
+      }
+    });
+  }
 
 }
